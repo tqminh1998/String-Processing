51f72765d7cd0e35de381ce197d4a9c3e3c76c24
tqminh1998 <30286786+tqminh1998@users.noreply.github.com>
2017-10-24 18:38:35 -0700

    Update DFASearching.cpp

--- DFASearching.cpp
+++ DFASearching.cpp
@@ -3,21 +3,26 @@
 #include <algorithm>
 using namespace std;
 
-int W[] = { 'a', 'b', 'c' };
+char W[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z', ' '};
 int sz_W = sizeof(W) / sizeof(W[0]);
 int DFA[1000][256];
 
 bool isSuffix(string P, int k, int q, char a){
-	
-	if (k >= q)
+
+	if (k > q + 1)
 		return false;
+
 	int j = q - 1;
-	for (int i = k - 2; k >= 0; k--){
+
+	for (int i = k - 2; i >= 0; i--){
 		if (P[i] != P[j])
 			return false;
 
 		j--;
 	}
+
+
 
 	return P[k - 1] == a;
 }
@@ -29,32 +34,38 @@ void ComputeTable(string P){
 			int k = min(m + 1, q + 2);
 			do{
 				k--;
-			} while (isSuffix(P, k, q, W[j]));
+			} while (isSuffix(P, k, q, W[j]) == false && k != 0);
+
 			DFA[q][W[j]] = k;
 		}
 	}
 }
 
-void DFASearch(string T, string P){
-	
+int DFASearch(string T, string P){
+
 	int q = 0;
 	for (int i = 0; i < T.size(); i++){
 		q = DFA[q][T[i]];
 		if (q == P.size()) {
-			cout << i - P.size() + 1;
-			return;
+			return i - P.size() + 1;
 		}
 	}
 
+    return -1;
 }
 
 int main(){
 
 	string P, T;
-	cin >> P;
-	cin >> T;
+	cout << "Nhap chuoi T: ";
+	getline(cin, T);
+	cout << "Nhap chuoi P: ";
+	getline(cin, P);
 
-	DFASearch(T, P);
+
+	ComputeTable(P);
+
+    cout << "Vi tri xuat hien dau tien cua P trong T la: " << DFASearch(T, P);
 
 	return 0;
 }
